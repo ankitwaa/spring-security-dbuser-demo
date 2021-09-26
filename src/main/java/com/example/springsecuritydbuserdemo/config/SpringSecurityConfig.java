40daf0f73b956382228945226845992e14587d4d
ankit <ankitforstudy@gmail.com>
2021-09-26 15:56:18 +0530

    SpringSecurity Configuration by Overriding configure method of WebSecurityConfigurerAdapter

--- src/main/java/com/example/springsecuritydbuserdemo/config/SpringSecurityConfig.java
+++ src/main/java/com/example/springsecuritydbuserdemo/config/SpringSecurityConfig.java
@@ -9,7 +9,7 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
-@Configuration̥
+@Configuration
 public class SpringSecurityConfig {
 
     @Bean
